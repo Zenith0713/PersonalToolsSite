6f475c3164a52e5daf8b96b89634aeb89705fca2
Axel Brown <axel.brown@eticoncept.com>
2021-09-24 14:03:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset=“UTF-8”>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mes outils</title>
+
+    <!-- Lien CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" type="text/css" href="/css/bootstrap-4.5.3.min.css"> -->
+</head>
+
+<body class="container w-100 mw-100">
+    <section class="row">
+        <article class="col bg-primary">
+            <h2>Todo :</h2>
+            <p>Liste de tache modifiable (chose a faire, course, etc...)</p>
+        </article>
+
+        <article class="col bg-secondary">
+            <h2>Muscu</h2>
+            <p>suivi de poid, taille/ programme muscu (graphique)</p>
+        </article>
+    </section>
+
+    <section class="row">
+        <article class="col bg-success">
+            <h2>Jeux vidéo</h2>
+            <p>suivi rank ow, aide autre jeux (ark tableau)</p>
+        </article>
+
+        <article class="col bg-danger">
+            <h2>Anime/Manga/Weebtoon</h2>
+            <p>un peu comme myanimelist (regarder api myanimelist "https://myanimelist.net/apiconfig/references/api/v2#tag/anime")</p>
+        </article>
+    </section>
+
+    <section class="row">
+        <article class="col bg-warning">
+            <h2>Argent</h2>
+            <p>Gestion des virements, données des factures (éléct, fortfait internet, etc...)</p>
+        </article>
+
+        <article class="col bg-info">
+            <h2>Test</h2>
+            <p>test, nouvelles choses que je veux faire</p>
+        </article>
+    </section>
+
+    <section class="row">
+        <article class="col bg-light">
+            <h2>Autres</h2>
+            <p>tout le reste</p>
+        </article>
+    </section>
+
+    <!-- <p>(peut etre se servir d'autres api, fonctionnalitées non communes)</p>
+
+    <button type="button" class="btn btn-primary">Send</button> -->
+
+    <!-- All Script -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
