d7080b52adb8b2b79e48095e2334a46cb22f6632
Axel Brown <axel.brown@eticoncept.com>
2021-09-30 17:10:05 +0200

    Modifications de la page d'accueil et stylisation du header

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset=“UTF-8”>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mes outils</title>
+
+    <!-- Lien CSS -->
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Todo</a></li>
+                <li><a href="#">Musculation</a></li>
+                <li><a href="#">Jeux vidéos</a></li>
+                <li><a href="#">Animes</a></li>
+                <li><a href="#">Finances</a></li>
+                <li><a href="#">Autres</a></li>
+            </ul>
+        </nav>
+    </header>
